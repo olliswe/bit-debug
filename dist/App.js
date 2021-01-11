8992e99e9816f19e5181c2743899740d2b615861
Oliver Iyer <oliver.iyer@livingpackets.com>
2021-01-11 10:31:25 +0100

    fix

--- dist/App.js
+++ dist/App.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import HelloWorld from 'components/HelloWorld';
-
 function App() {
-  return (
-    <div>
-      <HelloWorld/>
-    </div>
-  );
+    return (React.createElement("div", null,
+        React.createElement(HelloWorld, null)));
 }
-
 export default App;
